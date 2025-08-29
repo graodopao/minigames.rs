16212673332c23a732a9f6ddc06909780947a1e0
Tiago Costa Araujo <37983882+graodopao@users.noreply.github.com>
2025-08-29 05:57:47 -0300

    Added raylib dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut raylib, thread) = raylib::init()
+        .size(640, 480)
+        .title("graodopao's minigames")
+        .build();
+
+    while !raylib.window_should_close() {
+        let mut draw_handle = raylib.begin_drawing(&thread);
+
+        draw_handle.clear_background(Color::WHITE);
+        draw_handle.draw_text("Test", 12, 12, 20, Color::BLACK);
+    }
 }
